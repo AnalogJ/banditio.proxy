298adc1b2885581ead0970c8f89ae27d54dab742
Jason Kulatunga <jk17@ualberta.ca>
2013-09-15 11:29:23 -0700

    cleanup unneeded methods.

--- middleware/inject/index.js
+++ middleware/inject/index.js
@@ -1,9 +1,7 @@
 //based off the work of https://github.com/btotr/node-inject
 module.exports = function(script) {
     return function(req, res, next) {
-        //console.log('middleware', arguments)
         var _write = res.write,
-            _end = res.end,
             _writeHead = res.writeHead,
             isHtml = false;
 
@@ -27,22 +25,11 @@ module.exports = function(script) {
         }
 
         res.write = function(chunk, encoding) {
-            console.log("write", isHtml)
-            /*if (isHtml) {
-                chunk = chunk.toString().replace(/(<head[^>]*>)/, "$1" + scriptElm)
-            }
-            _write.call(res, chunk);
-
-            */
             if (isHtml) {
                 var str = chunk.toString();
-                console.log(str);
-                //var baseTag = '<base href="' + (dest.replace(/\/$/, '') || '') + '"/>';
 
                 str = str.replace(/(<head[^>]*>)/, "$1"  + scriptElm );
-
                 data = new Buffer(str);
-                //data = chunk;
             }
 
             _write.call(res, data, encoding);
